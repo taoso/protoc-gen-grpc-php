76d5d914b6cf67edfdc806d8a32a8eaa9bd0e87b
吕海涛 <i@lvht.net>
2018-02-23 17:17:33 +0800

    update example

--- example/gen.sh
+++ example/gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf sdk
 mkdir -p sdk
-protoc --php_out=sdk --plugin=protoc-gen-grpc-php=./vendor/bin/protoc-gen-grpc-php --grpc-php_out=composer_name=lvht/hello-sdk:sdk ./helloworld.proto
+protoc --my-php_out=base_message='\Lv\Grpc\Message':sdk --plugin=protoc-gen-my-php=../vendor/bin/protoc-gen-plugin.php --plugin=protoc-gen-grpc-php=./vendor/bin/protoc-gen-grpc-php --grpc-php_out=composer_name=lvht/hello-sdk:sdk ./helloworld.proto
 git init sdk > /dev/null
--- example/server.php
+++ example/server.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-$s = new Lv\Grpc\SwooleServer('127.0.0.1', 50051);
+$s = new Lv\Grpc\SwooleServer('127.0.0.1', 8080);
 $s->addService(new Lv\Grpc\Demo\GreeterService);
 
 $s->run();
